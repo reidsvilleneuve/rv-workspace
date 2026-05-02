1dd274687661280cf4f7dacee6c8c61c92853507
Reid Villeneuve <reidsvilleneuve@gmail.com>
2026-05-01 20:10:31 -0600

    Add workspace template improvements

--- templates/project/config/zsh-config-workspace.zsh
+++ templates/project/config/zsh-config-workspace.zsh
@@ -6,12 +6,14 @@ export PATH="RVDEVREPLACE_projectdir/scripts/:${(q)PATH}"
 
 alias scrap="nvim RVDEVREPLACE_projectdir/docs/scratchpad.md"
 alias pdp="pushd RVDEVREPLACE_projectdir"
-alias pdr="pushd RVDEVREPLACE_projectdir/repos/RVDEVREPLACE_prefix"
+alias pdr="pushd RVDEVREPLACE_projectdir/repos/rvdev"
 alias pdwn="pushd RVDEVREPLACE_projectdir/docs/work-notes/in-progress"
 
 alias sgv="source RVDEVREPLACE_projectdir/variables/git-variables.sh"
 
 # Git
+
+sgv
 
 alias gscur="git switch \"${(q)RVDEVREPLACE_prefix_git_current}\""
 alias gspr="git switch \"${(q)RVDEVREPLACE_prefix_git_pr}\""
